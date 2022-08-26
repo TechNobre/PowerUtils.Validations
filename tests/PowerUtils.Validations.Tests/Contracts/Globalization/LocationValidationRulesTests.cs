73e63749f1ed29a2d1c327743fe966b956a95af4
Nelson Nobre <technobre@gmail.com>
2022-08-26 01:56:06 +0100

    feat: Add rule to validate latitude and longitude nullable

--- tests/PowerUtils.Validations.Tests/Contracts/Globalization/LocationValidationRulesTests.cs
+++ tests/PowerUtils.Validations.Tests/Contracts/Globalization/LocationValidationRulesTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using FluentAssertions;
+using PowerUtils.Validations.Contracts;
 using PowerUtils.Validations.Tests.Fakes.Validations.Globalization;
 using PowerUtils.Validations.Tests.Fakes.ValueObjects;
 using Xunit;
@@ -124,6 +125,194 @@ namespace PowerUtils.Validations.Tests.Contracts.Globalization
 
             // Act
             var act = new FakeLocationValidation(fake);
+
+
+            // Assert
+            act.Valid.Should()
+                .BeTrue();
+
+            act.Notifications.Should()
+                .BeEmpty();
+        }
+
+        [Fact]
+        public void BelowMin_ValidateLatitudeNullable_Invalid()
+        {
+            // Arrange
+            double? latitude = -91.145;
+
+
+            // Act
+            var act = new ValidationsContract<double?>(latitude)
+                .RuleFor("fakeLatitude")
+                .Latitude();
+
+
+            // Assert
+            act.Invalid.Should()
+                .BeTrue();
+
+            act.Notifications.First().Property
+                .Should()
+                    .Be("fakeLatitude");
+
+            act.Notifications.First().ErrorCode
+                .Should()
+                    .Be(ErrorCodes.GetMinFormatted(-90));
+        }
+
+        [Fact]
+        public void AboveMax_ValidateLatitudeNullable_Invalid()
+        {
+            // Arrange
+            double? latitude = 91.145;
+
+
+            // Act
+            var act = new ValidationsContract<double?>(latitude)
+                .RuleFor("fakeLatitude")
+                .Latitude();
+
+
+            // Assert
+            act.Invalid.Should()
+                .BeTrue();
+
+            act.Notifications.First().Property
+                 .Should()
+                     .Be("fakeLatitude");
+
+            act.Notifications.First().ErrorCode
+                .Should()
+                    .Be(ErrorCodes.GetMaxFormatted(90));
+        }
+
+        [Fact]
+        public void NullLatitude_ValidateLatitudeNullable_Valid()
+        {
+            // Arrange
+            double? latitude = null;
+
+
+            // Act
+            var act = new ValidationsContract<double?>(latitude)
+                .RuleFor("fakeLatitude")
+                .Latitude();
+
+
+            // Assert
+            act.Valid.Should()
+                .BeTrue();
+
+            act.Notifications.Should()
+                .BeEmpty();
+        }
+
+        [Fact]
+        public void ValidLatitude_ValidateLatitudeNullable_Valid()
+        {
+            // Arrange
+            double? latitude = 12.45124;
+
+
+            // Act
+            var act = new ValidationsContract<double?>(latitude)
+                .RuleFor("fakeLatitude")
+                .Latitude();
+
+
+            // Assert
+            act.Valid.Should()
+                .BeTrue();
+
+            act.Notifications.Should()
+                .BeEmpty();
+        }
+
+        [Fact]
+        public void BelowMin_ValidateLongitudeNullable_Invalid()
+        {
+            // Arrange
+            double? longitude = -184.145;
+
+
+            // Act
+            var act = new ValidationsContract<double?>(longitude)
+                .RuleFor("fakeLongitude")
+                .Longitude();
+
+
+            // Assert
+            act.Invalid.Should()
+                .BeTrue();
+
+            act.Notifications.First().Property
+                .Should()
+                    .Be("fakeLongitude");
+
+            act.Notifications.First().ErrorCode
+                .Should()
+                    .Be(ErrorCodes.GetMinFormatted(-180));
+        }
+
+        [Fact]
+        public void AboveMax_ValidateLongitudeNullable_Invalid()
+        {
+            // Arrange
+            double? longitude = 184.145;
+
+
+            // Act
+            var act = new ValidationsContract<double?>(longitude)
+                .RuleFor("fakeLongitude")
+                .Longitude();
+
+
+            // Assert
+            act.Invalid.Should()
+                .BeTrue();
+
+            act.Notifications.First().Property
+                .Should()
+                    .Be("fakeLongitude");
+
+            act.Notifications.First().ErrorCode
+                .Should()
+                    .Be(ErrorCodes.GetMaxFormatted(180));
+        }
+
+        [Fact]
+        public void NullLongitude_ValidateLongitudeNullable_Valid()
+        {
+            // Arrange
+            double? longitude = null;
+
+
+            // Act
+            var act = new ValidationsContract<double?>(longitude)
+                .RuleFor("fakeLongitude")
+                .Longitude();
+
+
+            // Assert
+            act.Valid.Should()
+                .BeTrue();
+
+            act.Notifications.Should()
+                .BeEmpty();
+        }
+
+        [Fact]
+        public void ValidLongitude_ValidateLongitudeNullable_Valid()
+        {
+            // Arrange
+            double? longitude = 45.4512;
+
+
+            // Act
+            var act = new ValidationsContract<double?>(longitude)
+                .RuleFor("fakeLongitude")
+                .Longitude();
 
 
             // Assert
